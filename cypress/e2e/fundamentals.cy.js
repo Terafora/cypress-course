24c3408d6424eda9c1543d918183f4f5e559d583
Charlotte Stone <charlie.stone649@gmail.com>
2025-05-13 09:37:30 +0100

    Fix accordion item click selector in fundamentals test for improved visibility checks

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -6,7 +6,9 @@ describe('Fundamentals test', () => {
   it('Accordion content test', () => {
     cy.visit('/fundamentals')
     cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
-    cy.get('[data-test="accordion-item-1"]').click()
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
     cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
   })
 })
